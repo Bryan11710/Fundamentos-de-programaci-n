95b26e4b77488eca3a0e2aae036a4736892f5f5e
BryanCr7 <brycordova13@gmail.com>
2024-09-20 18:32:57 -0500

    Deber de la semana 14

--- Semana 14/CalculoDescuentoPython.py
+++ Semana 14/CalculoDescuentoPython.py
@@ -1,10 +1,7 @@
 def calcular_descuento(monto_total, porcentaje_descuento=10):
 
-
     descuento = (monto_total * porcentaje_descuento) / 100
     return descuento
-
-
 # Programa principal
 if __name__ == "__main__":
 
